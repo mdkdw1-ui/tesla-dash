33fd359a948fcd5458770bbda74f54aca29ac3b5
mdkdw1-ui <mdkdw1@gmail.com>
2026-07-23 02:11:04 +0900

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/tesladash/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.tesladash
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+        webView.webViewClient = WebViewClient()
+
+        // assets 폴더 내 index.html 로드
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
